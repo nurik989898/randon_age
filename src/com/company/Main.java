1c948e34bde5a8f15e710993bbb4fe8db2840e02
Nurik <nturdukulov911@gmail.com>
2021-12-06 16:46:38 +0600

    There was some changes in code namely in methods.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,11 +2,19 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
+
+        generateRandomAge();
+
+    }
+
+    public static int generateRandomAge(){
         int min = 1;
         int max = 100;
         System.out.println("Random age between about "+ min+"to"+max+":");
         int b = (int) (Math.random()*(max-min+ min)+min);
         System.out.println(b);
+        return b;
     }
-
 }
+
+
